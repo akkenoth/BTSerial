a399fedc747ced8ed452634cd1addbba837548b2
mjbogusz <mjbogusz+github@gmail.com>
2016-03-12 01:29:45 +0100

    Command: class created, it will be used for storing available commands (extends QListWidgetItem)

---
+++ Commands/Command.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import QListWidgetItem
+
+class Command(QListWidgetItem):
+	name = None
+	code = None
+	description = None
+
+	def __init__(self, name, code, description = ""):
+		QListWidgetItem.__init__(self)
+		self.name = name
+		self.code = code
+		self.description = description
+		self.setText(self.name + " ["+ self.code + "]")
+		self.setToolTip(self.description)
