0e45ebd032a65160d416e7f665c48d9eb50f19cc
mjbogusz <mjbogusz+github@gmail.com>
2016-03-13 15:54:06 +0100

    Commands: CommandType created, CommandItem functionality modified to match its name and added parameter parsing into code.

--- Commands/CommandItem.py
+++ Commands/CommandItem.py
@@ -1,14 +1,27 @@
 from PyQt5.QtWidgets import QListWidgetItem
+from Commands.CommandType import CommandType
 
 class CommandItem(QListWidgetItem):
 	name = None
 	code = None
 	description = None
 
-	def __init__(self, name, code, description = ""):
+	def __init__(self, commandType, *parameters):
+		if not isinstance(commandType, CommandType):
+			raise TypeError("CommandItem: commandType should be an instance of CommandType class.")
+		self.name = commandType.name
+		text = self.name + "("
+		for p in parameters:
+			text += p + ", "
+		text = text[:-2] + ")"
+
+		try:
+			self.code = commandType.code % parameters
+		except TypeError as e:
+			raise TypeError("CommandItem: one or more parameters didn't match expected types: " + e.msg)
+		self.code.encode('ascii', 'ignore')
+		self.description = commandType.description
+
 		QListWidgetItem.__init__(self)
-		self.name = name
-		self.code = code
-		self.description = description
-		self.setText(self.name + " ["+ self.code + "]")
-		self.setToolTip(self.description)
+		self.setText(text)
+		self.setToolTip(self.code + " | " + self.description)
