76e62e67a3183e5f06e6ae4ba7f981de430044d3
mjbogusz <mjbogusz+github@gmail.com>
2016-03-14 23:06:38 +0100

    Commands: CommandType: added support for command code passed as a list of bytes/params, added basic typechecks

--- Commands/CommandType.py
+++ Commands/CommandType.py
@@ -4,11 +4,24 @@ class CommandType(QListWidgetItem):
 	name = None
 	code = None
 	description = None
+	textType = True
 
-	def __init__(self, name, code, description = ""):
+	def __init__(self, name, code, description = "", textType = True):
 		QListWidgetItem.__init__(self)
-		self.name = name
-		self.code = code
-		self.description = description
-		self.setText(self.name + " ["+ self.code + "]")
+		self.name = str(name)
+		self.description = str(description)
+
+		if (textType is True) or (type(code) is str):
+			self.code = str(code)
+		elif type(code) is list:
+			self.code = ""
+			for i in code:
+				if i[0] == '%':
+					self.code += str(i)
+				elif (i[0] == '0') and (i[1] == 'x'):
+					self.code += chr(int(i, 16))
+		else:
+			raise TypeError("Bad code structure")
+
+		self.setText(self.name + " ["+ str(self.code) + "]")
 		self.setToolTip(self.description)
