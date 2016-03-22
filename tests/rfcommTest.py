56af0bb95553a3034cd5c9428648604351ee9746
mjbogusz <mjbogusz+github@gmail.com>
2016-03-22 17:40:18 +0100

    rfcommTest: fixed chr() usage on chars, added catching curses exceptions

--- tests/rfcommTest.py
+++ tests/rfcommTest.py
@@ -4,30 +4,34 @@ import curses
 port = 3
 
 def main():
-
-	(deviceAddr, deviceName) = getDevices()
-	if deviceAddr is None:
+	robot = connect()
+	if robot is None:
 		input("Press any key to close...")
 		return
 
-	mainLoop(deviceAddr, deviceName)
+	stdscr = setupCurses()
+	try:
+		mainLoop(robot, stdscr)
+	except Exception as e:
+		curses.endwin()
+		print("An error occured in main loop: " + str(e))
+	curses.endwin()
+	robot.close()
 
-	return
-
-def getDevices():
+def connect():
 	print("Scanning for devices (3s)...")
 	devices = []
 	try:
 		devices = bluetooth.discover_devices(duration = 3, lookup_names = True)
 	except Exception as e:
 		print("Error: %s\n" % str(e))
-		return (None, None)
+		return None
 
 	devicesLength = len(devices)
 
 	print("Found %d devices!" % devicesLength)
 	if len(devices) is 0:
-		return (None, None)
+		return None
 	i = 0
 	for addr, name in devices:
 		print("%d: %s [%s]" % (i, name, addr))
@@ -38,15 +42,13 @@ def getDevices():
 		key = int(key)
 	except Exception as e:
 		print("Bad input!")
-		return (None, None)
+		return None
 	if (key < 0) or (key > devicesLength):
 		print("Bad input!")
-		return (None, None)
+		return None
 
-	return devices[key]
-def mainLoop(deviceAddr, deviceName):
+	(deviceAddr, deviceName) = devices[key]
 	print("Searching for services on device: %s [%s]" % (deviceName, deviceAddr))
-
 	services = bluetooth.find_service(address = deviceAddr)
 
 	port = None
@@ -56,7 +58,7 @@ def mainLoop(deviceAddr, deviceName):
 			port = int(key)
 		except Exception as e:
 			print("Bad input!")
-			return
+			return None
 	else:
 		for i in range(len(services)):
 			print("%d: %s - Port: %s" % (i, services[i]["name"], services[i]["port"]))
@@ -65,40 +67,42 @@ def mainLoop(deviceAddr, deviceName):
 			key = int(key)
 		except Exception as e:
 			print("Bad input!")
-			return
+			return None
 		port = services[key]["port"]
 
 	print("Connecting...")
 	robot = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 	robot.connect((deviceAddr, port))
+	return robot
 
+def setupCurses():
 	stdscr = curses.initscr()
 	curses.cbreak()
 	stdscr.keypad(1)
 	stdscr.nodelay(True)
+	return stdscr
 
+def mainLoop(robot, stdscr):
 	keyUp = keyDown = keyLeft = keyRight = False
 	while True:
 		key = stdscr.getch()
 		if key is ord('q'):
 			break
 		elif key is -1:
-			robot.send(chr('L') + chr(0) + chr(0) + chr(0))
-			robot.send(chr('R') + chr(0) + chr(0) + chr(0))
+			robot.send('L' + chr(0) + chr(0) + chr(0))
+			robot.send('R' + chr(0) + chr(0) + chr(0))
 		elif key is ord('w'):
-			robot.send(chr('L') + chr(191) + chr(0) + chr(0))
-			robot.send(chr('R') + chr(191) + chr(0) + chr(0))
+			robot.send('L' + chr(191) + chr(0) + chr(0))
+			robot.send('R' + chr(191) + chr(0) + chr(0))
 		elif key is ord('s'):
-			robot.send(chr('L') + chr(64) + chr(0) + chr(0))
-			robot.send(chr('R') + chr(64) + chr(0) + chr(0))
+			robot.send('L' + chr(64) + chr(0) + chr(0))
+			robot.send('R' + chr(64) + chr(0) + chr(0))
 		elif key is ord('a'):
-			robot.send(chr('L') + chr(64) + chr(0) + chr(0))
-			robot.send(chr('R') + chr(191) + chr(0) + chr(0))
+			robot.send('L' + chr(64) + chr(0) + chr(0))
+			robot.send('R' + chr(191) + chr(0) + chr(0))
 		elif key is ord('d'):
-			robot.send(chr('L') + chr(191) + chr(0) + chr(0))
-			robot.send(chr('R') + chr(64) + chr(0) + chr(0))
-	robot.close()
-
+			robot.send('L' + chr(191) + chr(0) + chr(0))
+			robot.send('R' + chr(64) + chr(0) + chr(0))
 
 if __name__ == '__main__':
 	main()
