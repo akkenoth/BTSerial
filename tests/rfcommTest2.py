326416b4300288a393ba0bdc625653eb3ad0948b
mjbogusz <mjbogusz+github@gmail.com>
2016-03-22 20:41:41 +0100

    rfcommTest2: fixed lSpd/rSpd vars initialization/value setting

--- tests/rfcommTest2.py
+++ tests/rfcommTest2.py
@@ -80,8 +80,8 @@ def mainLoop(robot):
 	window = pygame.display.set_mode((800, 600), 0, 32)
 	window.fill((0, 0, 0))
 
+	lSpd = rSpd = 128
 	while True:
-		lSpd = rSpd = 128
 		for event in pygame.event.get():
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_q:
